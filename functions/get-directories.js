5c4c39b2f387a3dbfa51d1ae2d8464c5fbbe99cb
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 19:16:27 +0800

    Create get-directories.js

---
+++ functions/get-directories.js
@@ -0,0 +1,9 @@
+// functions/get-directories.js
+export async function onRequestGet(context) {
+  const { env, request } = context;
+  const url = new URL(request.url);
+  const username = url.searchParams.get('username');
+  const keys = await env.webphostore.list({ prefix: `${username}_` });
+  const directories = keys.keys.map(key => key.name.split('_')[1]);
+  return new Response(JSON.stringify(directories), { status: 200 });
+}
