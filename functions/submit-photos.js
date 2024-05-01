1e0440cc795039a05acd082a69ec5c371d01662f
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 19:36:57 +0800

    Update submit-photos.js

--- functions/submit-photos.js
+++ functions/submit-photos.js
@@ -2,8 +2,8 @@
 export async function onRequestPost(context) {
   const { request, env } = context;
   const { username, directory, photoData } = await request.json();
-  let currentValue = await env.webphostore.get(`${username}_${directory}`);
+  let currentValue = await context.env.webphostore.get(`${username}_${directory}`);
   currentValue = currentValue ? currentValue + ';' + photoData : photoData;
-  await env.webphostore.put(`${username}_${directory}`, currentValue);
+  await context.env.webphostore.put(`${username}_${directory}`, currentValue);
   return new Response('Photos submitted', { status: 200 });
 }
