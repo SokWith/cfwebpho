b5d9ffa05349ccca71ae9c7b394935137d1bc9f8
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 20:21:02 +0800

    Update get-photos.js

--- functions/get-photos.js
+++ functions/get-photos.js
@@ -4,7 +4,10 @@ export async function onRequestGet(context) {
   const url = new URL(request.url);
   const username = url.searchParams.get('username');
   const directory = url.searchParams.get('directory');
+  // 从KV空间获取对应的密钥值
   const photosString = await context.env.webphostore.get(`${username}_${directory}`);
-  const photos = photosString ? photosString.split(';') : [];
-  return new Response(JSON.stringify(photos), { status: 200 });
+  // 将字符串按换行符分割成数组，每个元素是一个图片URL
+  const photoUrls = photosString ? photosString.split('\n') : [];
+  return new Response(JSON.stringify(photoUrls), { status: 200 });
 }
+
