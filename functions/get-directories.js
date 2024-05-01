49938d1af4f1ecaa1c36b1ed48d21e55444fed93
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 19:36:23 +0800

    Update get-directories.js

--- functions/get-directories.js
+++ functions/get-directories.js
@@ -3,7 +3,7 @@ export async function onRequestGet(context) {
   const { env, request } = context;
   const url = new URL(request.url);
   const username = url.searchParams.get('username');
-  const keys = await env.webphostore.list({ prefix: `${username}_` });
+  const keys = await context.env.webphostore.list({ prefix: `${username}_` });
   const directories = keys.keys.map(key => key.name.split('_')[1]);
   return new Response(JSON.stringify(directories), { status: 200 });
 }
