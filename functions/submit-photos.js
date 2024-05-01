b8918948b1191d73e2a1a5aab1fa46fc5b849025
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 20:21:22 +0800

    Update submit-photos.js

--- functions/submit-photos.js
+++ functions/submit-photos.js
@@ -3,9 +3,9 @@ export async function onRequestPost(context) {
   const { request, env } = context;
   const { username, directory, photoUrlsString } = await request.json();
   // 将新提交的URL添加到现有的URL列表中
-  let existingUrls = await env.webphostore.get(`${username}_${directory}`);
+  let existingUrls = await context.env.webphostore.get(`${username}_${directory}`);
   existingUrls = existingUrls ? existingUrls + '\n' + photoUrlsString : photoUrlsString;
   // 更新KV空间中的密钥值
-  await env.webphostore.put(`${username}_${directory}`, existingUrls);
+  await context.env.webphostore.put(`${username}_${directory}`, existingUrls);
   return new Response('Photos submitted', { status: 200 });
 }
