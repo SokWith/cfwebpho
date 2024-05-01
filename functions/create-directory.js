e210f8bf4352aad8369d527de008c1fb0e615c4c
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 19:15:57 +0800

    Create create-directory.js

---
+++ functions/create-directory.js
@@ -0,0 +1,7 @@
+// functions/create-directory.js
+export async function onRequestPost(context) {
+  const { request, env } = context;
+  const { username, dirName } = await request.json();
+  await env.webphostore.put(`${username}_${dirName}`, '');
+  return new Response('Directory created', { status: 200 });
+}
