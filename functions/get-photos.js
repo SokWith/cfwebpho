f94059aed0fbbcf770888861378fc79689f4ca01
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 19:59:39 +0800

    Update get-photos.js

--- functions/get-photos.js
+++ functions/get-photos.js
@@ -4,10 +4,7 @@ export async function onRequestGet(context) {
   const url = new URL(request.url);
   const username = url.searchParams.get('username');
   const directory = url.searchParams.get('directory');
-  const photosString = await env.webphostore.get(`${username}_${directory}`);
-  const photos = photosString.split(';').map(entry => {
-    const [url, name] = entry.split(',');
-    return { url, name };
-  });
+  const photosString = await context.env.webphostore.get(`${username}_${directory}`);
+  const photos = photosString ? photosString.split(';') : [];
   return new Response(JSON.stringify(photos), { status: 200 });
 }
