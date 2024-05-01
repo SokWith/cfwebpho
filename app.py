b79468b2d4a5c03eee41805a8491f31b872e61db
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-01 17:32:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 # 以下是一个详细的cf pages应用的伪代码示例：
 
 # 欢迎页面
@@ -6,51 +8,52 @@ if len(username) < 6:
     print("用户名长度需大于等于6个字符")
 else:
     # 进入相册界面
-    directory_list = get_directory_list(username)
+    kv_space_name = os.getenv("webphostore")
+    directory_list = get_directory_list(kv_space_name, username)
     display_directory_list(directory_list)
     
     # 创建目录
     create_directory_input = input("请输入目录名：")
     create_directory_button = Button("创建目录")
     if create_directory_button.clicked:
-        create_directory(username, create_directory_input)
+        create_directory(kv_space_name, username, create_directory_input)
         refresh_page()
 
 # 相册管理界面
-def open_album(username, directory_name):
-    album_data = get_album_data(username, directory_name)
+def open_album(kv_space_name, username, directory_name):
+    album_data = get_album_data(kv_space_name, username, directory_name)
     display_album(album_data)
     
     # 添加照片
     add_photo_input = input("请输入图片URL地址，图片名称：")
     add_photo_button = Button("批量提交图片")
     if add_photo_button.clicked:
-        add_photos_to_directory(username, directory_name, add_photo_input)
+        add_photos_to_directory(kv_space_name, username, directory_name, add_photo_input)
         refresh_page()
 
 # 从KV空间提取密钥和值的操作
-def get_directory_list(username):
-    keys = get_keys_from_kv(username)
+def get_directory_list(kv_space_name, username):
+    keys = get_keys_from_kv(kv_space_name, username)
     directory_list = []
     for key in keys:
         directory_name = key.split("_", 1)[1]
         directory_list.append(directory_name)
     return directory_list
 
-def get_album_data(username, directory_name):
+def get_album_data(kv_space_name, username, directory_name):
     key = username + "_" + directory_name
-    album_data = get_value_from_kv(key)
+    album_data = get_value_from_kv(kv_space_name, key)
     return album_data
 
-def create_directory(username, directory_name):
+def create_directory(kv_space_name, username, directory_name):
     key = username + "_" + directory_name
-    create_key_in_kv(key, "")
+    create_key_in_kv(kv_space_name, key, "")
 
-def add_photos_to_directory(username, directory_name, photo_input):
+def add_photos_to_directory(kv_space_name, username, directory_name, photo_input):
     key = username + "_" + directory_name
     photos = photo_input.split(";")
     for photo in photos:
-        add_photo_to_kv(key, photo)
+        add_photo_to_kv(kv_space_name, key, photo)
 
 # 显示照片墙
 def display_album(album_data):
