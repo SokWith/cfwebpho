116f4aaa8a79ab0fd188718f064d37c5abf9af6f
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-02 01:00:24 +0800

    Update album-management.js

--- album-management.js
+++ album-management.js
@@ -1,10 +1,8 @@
 // album-management.js
+// JavaScript函数，用于上传单个图片并返回完整的URL地址
 function uploadImageAndGetFullUrl(uploadEndpoint, hostUrl, file) {
-    return new Promise((resolve, reject) => {
-        const formData = new FormData();
-        formData.append('file', file);
-
-        // 动态创建加载动画元素并添加到页面中
+    const formData = new FormData();
+    // 动态创建加载动画元素并添加到页面中
     const loadingElement = document.createElement('div');
     loadingElement.id = 'loading';
     loadingElement.textContent = '图片加载中......';
@@ -23,48 +21,59 @@ function uploadImageAndGetFullUrl(uploadEndpoint, hostUrl, file) {
     loadingElement.style.zIndex = '1000';
     //loadingElement.style.animation = 'blink 1s linear infinite';
 
-        // 检查文件大小，如果大于5MB，则压缩
-        const uphostUrl = 'https://testupimg.wook.eu.org';
-        if (file.size > 5242880) { // 1MB
-            imageCompression(file, { maxSizeMB: 5 })
-                .then(compressedFile => {
-                    formData.set('file', compressedFile);
-                    return fetch(`${uphostUrl}${uploadEndpoint}`, {
-                        method: 'POST',
-                        body: formData
-                    });
-                })
-                .then(response => response.json())
-                .then(data => {
-                    document.body.removeChild(loadingElement); // 移除加载动画
-                    if (data.error) {
-                        throw new Error(data.error);
-                    }
-                    resolve(`${hostUrl}${data[0].src}`);
-                })
-                .catch(error => {
-                    document.body.removeChild(loadingElement); // 移除加载动画
-                    reject(error);
-                });
-        } else {
-            fetch(`${hostUrl}${uploadEndpoint}`, {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                document.body.removeChild(loadingElement); // 移除加载动画
-                if (data.error) {
-                    throw new Error(data.error);
-                }
-                resolve(`${hostUrl}${data[0].src}`);
-            })
-            .catch(error => {
-                document.body.removeChild(loadingElement); // 移除加载动画
-                reject(error);
-            });
-        }
+    // 图片压缩功能
+  function handleCompressFile(file) {
+  const maxFileSize = 5 * 1024 * 1024; // 5MB
+  return new Promise((resolve) => {
+    if (file.size <= maxFileSize || !file.type.startsWith("image")) {
+      resolve(file);
+    } else {
+      imageCompression(file, { maxSizeMB: 5 })
+        .then((compressedFile) => {
+          resolve(compressedFile);
+        })
+        .catch((error) => {
+          console.error(">> imageCompression error", error);
+          resolve(file);
+        });
+    }
+  });
+}
+
+  
+
+    // 压缩图片并上传
+  const uphostUrl = 'https://testupimg.wook.eu.org';
+    //'https://testupimg.sokwith.workers.dev'
+    return handleCompressFile(file).then(compressedFile => {
+        formData.append("file", compressedFile);
+        return fetch(`${uphostUrl}${uploadEndpoint}`, {
+            method: 'POST',
+            body: formData
+        }).finally(() => {
+            // 移除加载动画元素
+            document.body.removeChild(loadingElement);
+        });
     });
+}
+
+  
+
+    // 压缩图片并上传
+  const uphostUrl = 'https://testupimg.wook.eu.org';
+    //'https://testupimg.sokwith.workers.dev'
+    return handleCompressFile(file).then(compressedFile => {
+        formData.append("file", compressedFile);
+        return fetch(`${uphostUrl}${uploadEndpoint}`, {
+            method: 'POST',
+            body: formData
+        }).finally(() => {
+            // 移除加载动画元素
+            document.body.removeChild(loadingElement);
+        });
+    });
+}
+
 }
 
 function setupFileInputAndUpload(uploadEndpoint, hostUrl) {
@@ -86,26 +95,27 @@ function setupFileInputAndUpload(uploadEndpoint, hostUrl) {
     });
 
     realFileInput.addEventListener('change', function(event) {
-        const files = event.target.files;
-    if (files.length > 0) {
-        // 处理文件上传的逻辑
-            const uploadPromises = Array.from(files).map(file => {
-            return uploadImageAndGetFullUrl(uploadEndpoint, hostUrl, file);
-    } 
+    const files = event.target.files;
+    const uploadPromises = Array.from(files).map(file => {
+        return uploadImageAndGetFullUrl(uploadEndpoint, hostUrl, file);
+    });
 
-        Promise.all(uploadPromises)
-            .then(fullUrls => {
-                photoUrlInput.value = fullUrls.join('\n');
-                // 这里可以添加提交表单的逻辑
-            })
-            .catch(error => {
-                console.error('上传失败:', error);
+    Promise.all(uploadPromises)
+        .then(responses => {
+            const fullUrls = responses.map(data => {
+                if (data.error) {
+                    throw new Error(data.error);
+                }
+                return `${hostUrl}${data[0].src}`; // 使用 data[0].src 获取第一个图片的URL
             });
-    }else {
-        // 如果没有选择文件，确保不会执行其他操作
-        event.preventDefault();
-    }
+            photoUrlInput.value = fullUrls.join('\n');
+            // 这里可以添加提交表单的逻辑
+        })
+        .catch(error => {
+            console.error('上传失败:', error);
         });
+});
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
