4150732e40596958bdadbd69de417d89713c1840
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 07:24:55 +0800

    Update putdir.js

--- functions/db/putdir.js
+++ functions/db/putdir.js
@@ -15,12 +15,22 @@ export async function onRequest(context) {
   }
 
     // 检测目录是否存在
-  const yesdir = await fetch(`./db/getdir?username=${username}`);
+ // const yesdir = await fetch(`./db/getdir?username=${username}`);
   
   // 如果yesdir响应正常，则返回 "Dir is here!"
-  if (yesdir.ok) {
-    return new Response('Dir is here!', { status: 200 });
-  }
+  //if (yesdir.ok) {
+  //  return new Response('Dir is here!', { status: 200 });
+ // }
+  // 构建SQL查询语句
+  const query = 'SELECT ad_name FROM webphostore WHERE ad_name = ?';
+
+  // 执行查询并等待结果
+  try {
+    const ps = await database.prepare(query).bind(fullname);
+    const result = await ps.raw();
+    const formattedResult = result.map(row => row[0].split('_')[1]);
+    return new Response(JSON.stringify(formattedResult), { status: 200 });
+  }else {
   
  // 构建SQL插入语句
   const query = 'INSERT INTO webphostore (ad_name, imgURL) VALUES (?, ?)';
@@ -32,6 +42,10 @@ export async function onRequest(context) {
     return new Response('Data inserted successfully.', { status: 200 });
   } catch (error) {
     // 如果插入过程中出现错误，返回错误信息
+    return new Response(error.message, { status: 500 });
+  }
+  } catch (error) {
+    // 如果查询过程中出现错误，返回错误信息
     return new Response(error.message, { status: 500 });
   }
 }
