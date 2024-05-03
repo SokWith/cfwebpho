69b602f4de41c2b3bbd91194b4d3d10bb287b457
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 07:44:43 +0800

    Update putdir.js

--- functions/db/putdir.js
+++ functions/db/putdir.js
@@ -6,44 +6,42 @@ export async function onRequest(context) {
   const username = url.searchParams.get('username');
   let fullname = username + '_' + directory;
   const imgUrl = url.searchParams.get('imgURL') || '';
-  
-  
 
   // 检查数据库连接是否已定义
   if (!database) {
     return new Response('Database connection not found.', { status: 500 });
   }
 
-    // 检测目录是否存在
- // const yesdir = await fetch(`./db/getdir?username=${username}`);
-  
-  // 如果yesdir响应正常，则返回 "Dir is here!"
-  //if (yesdir.ok) {
-  //  return new Response('Dir is here!', { status: 200 });
- // }
+  // 检测目录是否存在
   // 构建SQL查询语句
   const fquery = 'SELECT ad_name FROM webphostore WHERE ad_name = ?';
 
   // 执行查询并等待结果
+  let result;
   try {
-    const ps = await database.prepare(fquery).bind(fullname);
-    const result = await ps.raw();
-  }
-  if (result){
-    return new Response('Dir is here!', { status: 200 });
-  }else {
-  
- // 构建SQL插入语句
-  const query = 'INSERT INTO webphostore (ad_name, imgURL) VALUES (?, ?)';
-
-  // 执行插入并等待结果
-  try {
-    const ps = await database.prepare(query).bind(fullname, imgUrl);
-    await ps.run();
-    return new Response('Data inserted successfully.', { status: 200 });
+    const ps = await database.prepare(fquery);
+    await ps.bind(fullname);
+    result = await ps.all();
   } catch (error) {
-    // 如果插入过程中出现错误，返回错误信息
+    // 如果查询过程中出现错误，返回错误信息
     return new Response(error.message, { status: 500 });
   }
+
+  // 如果目录存在，返回确认信息
+  if (result.length > 0){
+    return new Response('Dir is here!', { status: 200 });
+  } else {
+    // 构建SQL插入语句
+    const query = 'INSERT INTO webphostore (ad_name, imgURL) VALUES (?, ?)';
+
+    // 执行插入并等待结果
+    try {
+      const ps = await database.prepare(query);
+      await ps.bind(fullname, imgUrl).run();
+      return new Response('Data inserted successfully.', { status: 200 });
+    } catch (error) {
+      // 如果插入过程中出现错误，返回错误信息
+      return new Response(error.message, { status: 500 });
+    }
   }
 }
