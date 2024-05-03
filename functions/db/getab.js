8140c44267868939be6979fcd1b508e42cc5014c
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-03 23:02:37 +0800

    Update getab.js

--- functions/db/getab.js
+++ functions/db/getab.js
@@ -18,16 +18,16 @@ export async function onRequest(context) {
   // 执行查询并等待结果
   try {
     const ps = await database.prepare(query).bind(username);
-   // const photosString = await ps.raw();
-    const photosStringArray = await ps.raw();
+    const photosString = await ps.raw();
+   // const photosStringArray = await ps.raw();
   
-  const photosString  = photosStringArray.first(); // 假设字符串在数组的第一个位置
-   //   const photosStringtp = typeof photosString; 
+  //const photosString  = photosStringArray.first(); // 假设字符串在数组的第一个位置
+      const photosStringtp = typeof photosString; 
 
     // 将字符串按换行符分割成数组，每个元素是一个图片URL
-  const result = photosString ? photosString.split('\n') : [];
-    return new Response(JSON.stringify(result), { status: 200 });
- //   return new Response( photosStringArray, { status: 200 });
+ // const result = photosString ? photosString.split('\n') : [];
+  //  return new Response(JSON.stringify(result), { status: 200 });
+   return new Response( photosStringtp, { status: 200 });
   } catch (error) {
     // 如果查询过程中出现错误，返回错误信息
     return new Response(error.message, { status: 500 });
