dc2941560858edc3a598b8fcb048331ecaed8114
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-03 15:53:28 +0800

    Update getdir.js

--- functions/db/getdir.js
+++ functions/db/getdir.js
@@ -1,7 +1,13 @@
 export async function onRequest(context) {
-  // Create a prepared statement with our query
-  const ps = context.env.webphostore.prepare('SELECT * from webphostore');
-  const data = await ps.first();
+  // 直接从环境变量中获取数据库连接
+  const database = context.env.webpho_db;
 
-  return Response.json(data);
+  // 构建SQL查询语句
+  const query = 'SELECT * FROM webphostore WHERE ad_name LIKE ?';
+
+  // 执行查询并等待结果
+  const result = await database.query(query, ['aaaaaa_%']);
+
+  // 返回查询结果
+  return new Response(JSON.stringify(result), { status: 200 });
 }
