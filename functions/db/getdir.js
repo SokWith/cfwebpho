c831d8b98f33443ab41d0409e61cd59723e01257
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-03 17:43:36 +0800

    Update getdir.js

--- functions/db/getdir.js
+++ functions/db/getdir.js
@@ -8,7 +8,7 @@ export async function onRequest(context) {
   }
 
   // 构建SQL查询语句
-  const query = 'SELECT ad_name FROM webphostore WHERE ad_name LIKE "aaaaaa_cc"';
+  const query = 'SELECT ad_name FROM webphostore WHERE ad_name LIKE "aaaaaa_bbb"';
 
   // 执行查询并等待结果
   try {
