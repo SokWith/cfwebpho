0dd29e181d50e3d34a842dbe5af110d0faa8c4d2
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-03 16:10:09 +0800

    Update getdir.js

--- functions/db/getdir.js
+++ functions/db/getdir.js
@@ -1,13 +1,21 @@
 export async function onRequest(context) {
-  // 直接从环境变量中获取数据库连接
+  // 从环境变量中获取数据库连接
   const database = context.env.webpho_db;
 
+  // 检查数据库连接是否已定义
+  if (!database) {
+    return new Response('Database connection not found.', { status: 500 });
+  }
+
   // 构建SQL查询语句
-  const query = 'SELECT * FROM webphostore WHERE ad_name LIKE ?';
+  const query = 'SELECT ad_name FROM webphostore WHERE ad_name LIKE ?';
 
   // 执行查询并等待结果
-  const result = await database.query(query, ['aaaaaa_%']);
-
-  // 返回查询结果
-  return new Response(JSON.stringify(result), { status: 200 });
+  try {
+    const result = await database.query(query, ['aaaaaa_%']);
+    return new Response(JSON.stringify(result), { status: 200 });
+  } catch (error) {
+    // 如果查询过程中出现错误，返回错误信息
+    return new Response(error.message, { status: 500 });
+  }
 }
