50646d3d5c54b0c3651008399bb93b2951fa9a36
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-03 15:47:12 +0800

    Update getdir.js

--- functions/db/getdir.js
+++ functions/db/getdir.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   // Create a prepared statement with our query
-  const ps = context.env.webphostore.prepare('SELECT ad_name FROM webphostore WHERE ad_name LIKE 'aaaaaa_%');
+  const ps = context.env.webphostore.prepare('SELECT * from webphostore');
   const data = await ps.first();
 
   return Response.json(data);
