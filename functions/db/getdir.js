7cc3fbc2e5d391038f234f051d9ac25ebb6905b7
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-03 15:39:29 +0800

    Update getdir.js

--- functions/db/getdir.js
+++ functions/db/getdir.js
@@ -1,18 +1,7 @@
-// functions/db/getdir.js
-export async function onRequestGet(context) {
-  // 解构环境变量和请求对象
-  const { webpho_db, request } = context;
-  const url = new URL(request.url);
-  const username = url.searchParams.get('username');
+export async function onRequest(context) {
+  // Create a prepared statement with our query
+  const ps = context.env.webphostore.prepare('SELECT * from users');
+  const data = await ps.first();
 
-  // 构建查询D1数据库的SQL语句，以匹配以username_开头的ad_name列的值
-  const query = `SELECT ad_name FROM webphostore WHERE ad_name LIKE ?`;
-
-  // 执行查询并获取结果
-  const result = await context.env.webpho_db.query(query, [`${username}_%`]);
-
-  // 提取目录名
-  const directories = result.rows.map(row => row.ad_name);
-
-  return new Response(JSON.stringify(directories), { status: 200 });
+  return Response.json(data);
 }
