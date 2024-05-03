8b8a2d1a8a5a2f47e779d373ae97cf28732099aa
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-03 23:06:44 +0800

    Update getab.js

--- functions/db/getab.js
+++ functions/db/getab.js
@@ -23,6 +23,8 @@ export async function onRequest(context) {
   
   //const photosString  = photosStringArray.first(); // 假设字符串在数组的第一个位置
       const photosStringtp = typeof photosString; 
+    console.log(photosString);
+
 
     // 将字符串按换行符分割成数组，每个元素是一个图片URL
  // const result = photosString ? photosString.split('\n') : [];
