d55f59a66e8b09aca8b0f8f4d3415e55fb2ff9ab
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 15:20:38 +0800

    Update getdirpho.js

--- functions/db/getdirpho.js
+++ functions/db/getdirpho.js
@@ -1,9 +1,15 @@
 // functions/get-photos.js
 export async function onRequestGet(context) {
-  const { env, request } = context;
-  const url = new URL(request.url);
-  const username = url.searchParams.get('username');
-  const directory = url.searchParams.get('directory');
+ // 从环境变量中获取数据库连接
+  const database = context.env.webpho_db;
+  const { username, dirName } = await context.request.json();
+  const fullname = `${username}_${dirName}`;
+  const imgUrl = '';
+  
+  // 检查数据库连接是否已定义
+  if (!database) {
+    return new Response('Database connection not found.', { status: 500 });
+  }
   // 从KV空间获取对应的密钥值
   const photosString = await context.env.webphostore.get(`${username}_${directory}`);
   // 将字符串按换行符分割成数组，每个元素是一个图片URL
