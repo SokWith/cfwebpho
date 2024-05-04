3ebce916cb890d23a42fd2910190da4375521483
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 15:36:06 +0800

    Update getdirpho.js

--- functions/db/getdirpho.js
+++ functions/db/getdirpho.js
@@ -2,9 +2,9 @@
 export async function onRequestGet(context) {
  // 从环境变量中获取数据库连接
   const database = context.env.webpho_db;
-  const { username, dirName, photoUrl } = await context.request.json();
+  const { username, dirName } = await context.request.json();
   const fullname = `${username}_${dirName}`;
-  const imgUrl = '${photoUrl}';
+  //const imgUrl = '${photoUrl}';
   
   // 检查数据库连接是否已定义
   if (!database) {
