3d37c9f457c1c06baca956a9bd6ef89a9c02b4fb
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 22:47:38 +0800

    Update listdb-free.js

--- functions/db/listdb-free.js
+++ functions/db/listdb-free.js
@@ -20,23 +20,25 @@ export async function onRequest(context) {
     const ps = await database.prepare(query).bind(m);
     const photosStringArray = await ps.raw();
     
-  let photosString  = photosStringArray.map(item => item[0]); // 假设字符串在数组的第一个位置
+    let photosString  = photosStringArray.map(item => item[0]); // 假设字符串在数组的第一个位置
   // 使用 join() 方法将数组元素连接成一个字符串，然后用 split() 方法以回车符分割
-  const urls = photosString.join('\n').split('\n'); 
-    return new Response(JSON.stringify(urls), {
+    const allurls = photosString.join('\n').split('\n'); 
+    // 使用 filter() 方法移除所有空字符串
+    const urls = allurls.filter(url => url.trim() !== '');
+ //  return new Response(JSON.stringify(urls), {
+ //   headers: { 'Content-Type': 'application/json' },
+ //   status: 200
+//  });
+  // 如果获取的URL数量不足n个，则使用所获取的最大数量
+  const maxUrls = urls.length < n ? urls.length : n;
+
+  // 随机抽取n个URL
+  const selectedUrls = urls.sort(() => Math.random() - 0.5).slice(0, maxUrls);
+
+  return new Response(JSON.stringify(selectedUrls), {
     headers: { 'Content-Type': 'application/json' },
     status: 200
   });
-  // 如果获取的URL数量不足n个，则使用所获取的最大数量
-//  const maxUrls = urls.length < n ? urls.length : n;
-
-  // 随机抽取n个URL
-//  const selectedUrls = urls.sort(() => Math.random() - 0.5).slice(0, maxUrls);
-
-//  return new Response(JSON.stringify(selectedUrls), {
-//    headers: { 'Content-Type': 'application/json' },
-//    status: 200
-//  });
 
     
   } catch (error) {
