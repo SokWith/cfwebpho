af0312817661ed74a9c20219a4a75cb9bac5a07d
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 08:04:22 +0800

    Update postdir.js

--- functions/db/postdir.js
+++ functions/db/postdir.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
   //const directory = url.searchParams.get('directory');
   //let username = url.searchParams.get('username') + '_' + directory;
   //const imgUrl = url.searchParams.get('imgURL') || '';
-  const dirfull = username + '_' + dirName;
+  const fullname = username + '_' + dirName;
   const imgUrl = '';
   
   
@@ -16,16 +16,37 @@ export async function onRequest(context) {
     return new Response('Database connection not found.', { status: 500 });
   }
 
- // 构建SQL插入语句
-  const query = 'INSERT INTO webphostore (ad_name, imgURL) VALUES (?, ?)';
+ // 检测目录是否存在
+  // 构建SQL查询语句
+  const fquery = 'SELECT ad_name FROM webphostore WHERE ad_name = ?';
 
-  // 执行插入并等待结果
+  // 执行查询并等待结果
+  let result;
   try {
-    const ps = await database.prepare(query).bind(dirfull, imgUrl);
-    await ps.run();
-    return new Response('Data inserted successfully.', { status: 200 });
+    const ps = await database.prepare(fquery).bind(fullname);
+    //await ps.bind(fullname);
+    result = await ps.all();
+   // return new Response(JSON.stringify(result), { status: 200 });
   } catch (error) {
-    // 如果插入过程中出现错误，返回错误信息
+    // 如果查询过程中出现错误，返回错误信息
     return new Response(error.message, { status: 500 });
+  }
+
+  // 如果目录存在，返回确认信息
+  if (result.results && result.results.length > 0){
+    return new Response('Dir is here!', { status: 209 });
+  } else {
+    // 构建SQL插入语句
+    const query = 'INSERT INTO webphostore (ad_name, imgURL) VALUES (?, ?)';
+
+    // 执行插入并等待结果
+    try {
+      const ps = await database.prepare(query).bind(fullname, imgUrl);
+      await ps.run();
+      return new Response('Data inserted successfully.', { status: 200 });
+    } catch (error) {
+      // 如果插入过程中出现错误，返回错误信息
+      return new Response(error.message, { status: 500 });
+    }
   }
 }
