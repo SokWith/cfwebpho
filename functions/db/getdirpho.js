77ed481c1e2c2d596172b9838f030c174f3b77cc
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 15:28:15 +0800

    Update getdirpho.js

--- functions/db/getdirpho.js
+++ functions/db/getdirpho.js
@@ -10,9 +10,31 @@ export async function onRequestGet(context) {
   if (!database) {
     return new Response('Database connection not found.', { status: 500 });
   }
-  // 从KV空间获取对应的密钥值
-  const photosString = await context.env.webphostore.get(`${username}_${directory}`);
-  // 将字符串按换行符分割成数组，每个元素是一个图片URL
-  const photoUrls = photosString ? photosString.split('\n') : [];
-  return new Response(JSON.stringify(photoUrls), { status: 200 });
+   // 检查数据库连接是否已定义
+  if (!database) {
+    return new Response('Database connection not found.', { status: 500 });
+  }
+
+  // 构建SQL查询语句
+  const query = 'SELECT imgURL FROM webphostore WHERE ad_name = ?';
+
+  // 执行查询并等待结果
+  try {
+    const ps = await database.prepare(query).bind(username);
+   // const photosString = await ps.raw();
+    const photosStringArray = await ps.raw();
+  
+  const photosString  = photosStringArray[0][0]; // 假设字符串在数组的第一个位置
+  //    const photosStringtp = typeof photosString; 
+  //  console.log(photosString);
+
+
+    // 将字符串按换行符分割成数组，每个元素是一个图片URL
+  const result = photosString ? photosString.split('\n') : [];
+    return new Response(JSON.stringify(result), { status: 200 });
+  // return new Response( photosStringtp, { status: 200 });
+  } catch (error) {
+    // 如果查询过程中出现错误，返回错误信息
+    return new Response(error.message, { status: 500 });
+  }
 }
