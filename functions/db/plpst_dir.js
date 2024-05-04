9071a57ade0ad44b8bdd342825d85330a15cb8a9
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-04 12:54:45 +0800

    Update plpst_dir.js

--- functions/db/plpst_dir.js
+++ functions/db/plpst_dir.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   // 从环境变量中获取数据库连接
   const database = context.env.webpho_db;
  // const database = context.env.db_phostore;
-  const { username, dirName } = await request.json();
+  const { username, dirName } = await context.request.json();
   const fullname = username + '_' + dirName;
   const imgUrl = '';
 
