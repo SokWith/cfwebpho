4707371bdc32bf8419f86bfe7689502456e35392
SokWith <129967025+SokWith@users.noreply.github.com>
2024-05-08 08:35:00 +0800

    Update album-management.js

--- album-management.js
+++ album-management.js
@@ -67,8 +67,8 @@ function uploadImageAndGetFullUrl(uploadEndpoint, hostUrl, file) {
 }
 
 // 定义host URL和上传端点
-//const hostUrl = 'https://imghost.wook.eu.org';
-const hostUrl = 'https://telegra.ph';
+const hostUrl = 'https://upimg.wook.eu.org';
+//const hostUrl = 'https://telegra.ph';
 const uploadEndpoint = '/upload';
 
 // 在文档加载完成后添加事件监听器
